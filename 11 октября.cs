409477270e6c1a907dbfa6e4e983a43f6a6b0f77
maxximben <maximben888@gmail.com>
2024-10-11 09:13:54 +0600

    Create 11 октября.cs

---
+++ 11 октября.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            
+            int last_digit;
+            while (a > 0) 
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                int p = a;
+                int count_even = 0;
+                while (p != 0)
+                {
+                    last_digit = p % 10;
+                    if (last_digit % 2 == 0)
+                        count_even++;
+                    p /= 10;
+                }
+                if (count_even == 0)
+                { Console.WriteLine("четных нет"); continue; }
+
+                int s = 0;
+                int n = a;
+                int i = 1;
+                while (n != 0)
+                {
+                    last_digit = n % 10;
+                    if (last_digit % 2 == 0)
+                        s = (s + last_digit) * 10;
+                    n /= 10;
+                }
+                Console.WriteLine(s/10);
+                Console.WriteLine();
+            }
+        }
+    }
+}
