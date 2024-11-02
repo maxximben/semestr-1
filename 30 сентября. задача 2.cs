8ab5ce1546370e979915d99a1749c94ca5687c9c
maxximben <maximben888@gmail.com>
2024-11-02 16:56:31 +0600

    Create 30 сентября. задача 2.cs

---
+++ 30 сентября. задача 2.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ведите n: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int a; int len = 0;
+            int min = n + 1;
+            for (int i = 0; i < n; i++)
+            {
+                a = Int32.Parse(Console.ReadLine());
+                if (a == 0)
+                    len++;
+                else
+                {
+                    if (len > 0) min = Math.Min(len, min);
+                    len = 0;
+                }
+            }
+            if (len > 0) min = Math.Min(len, min);
+            Console.WriteLine("минимальная длина: " + min);
+        }
+    }
+}
